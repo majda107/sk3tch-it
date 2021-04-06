09e9f8eeab47578b420ea8ad6468e7fe4d612015
majda107 <majda.trpkos@gmail.com>
2021-04-06 23:47:05 +0200

    functional gsap

--- Sk3tchIt/ClientApp/src/components/Overlay.tsx
+++ Sk3tchIt/ClientApp/src/components/Overlay.tsx
@@ -1,16 +1,28 @@
-import { useContext } from "react"
+import { useContext, useEffect, useRef } from "react"
 import { OverlayContext } from "../context/overlay.context"
+import gsap from "gsap";
 
 import "./Overlay.css";
 
 export function Overlay(): JSX.Element | null {
 
     const ctx = useContext(OverlayContext);
+    const overlay = useRef({} as HTMLDivElement);
 
 
-    if (!ctx.text) return null;
+    useEffect(() => {
 
-    return <div className="overlay">
+        if (ctx.text)
+            gsap.from(overlay.current, { opacity: 0, duration: 0.5 });
+        else
+            gsap.from(overlay.current, { opacity: 1, duration: 0.5 });
+
+    }, [ctx.text]);
+
+
+    // if (!ctx.text) return null;
+
+    return <div className="overlay" ref={overlay} style={{ opacity: ctx.text ? 1 : 0 }}>
         <p>{ctx.text}</p>
     </div>
 }
